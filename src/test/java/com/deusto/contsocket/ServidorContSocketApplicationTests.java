351ccbd4145e9bed76563b2aad90dd6ccdd200ce
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-12-02 18:09:12 +0100

    creación del servidor ContSocket

---
+++ src/test/java/com/deusto/contsocket/ServidorContSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusto.contsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorContSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
